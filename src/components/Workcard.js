d2a0632d4e527299087c7f0d2c4efd6798c7bca3
Vaishnavi Gupta <vaish101navi@gmail.com>
2024-04-26 20:58:44 +0530

    test

--- src/components/Workcard.js
+++ src/components/Workcard.js
@@ -7,7 +7,7 @@ import { NavLink } from "react-router-dom";
 const Workcard = (props) => {
   return (
     <div className="project-card">
-    <img src={props.imgsrc} alt="image" />
+    <img src={props.imgsrc} alt="projectsimg" />
     <h2 className="project-title">{props.title}</h2>
     <div className="pro-details">
         <p>{props.text}</p>
